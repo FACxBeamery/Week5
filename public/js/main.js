ebbc2c9ad77465a0bff3b6f85d1cac8781e42e39
Lyndsey Scott <lyndseyscott@Lyndseys-MacBook-Pro.local>
2019-10-10 11:02:30 +0100

    results display on page

--- public/js/main.js
+++ public/js/main.js
@@ -1,59 +1,59 @@
 document.getElementById("submitButton").addEventListener("click", event => {
-    event.preventDefault();
-    const topicValue = document.getElementById("topic").value;
+	event.preventDefault();
+	const topicValue = document.getElementById("topic").value;
 
-    // //API CALL. CALL FUNCTION ON READY**************
-    // fetch(`/resources?topic=${topicValue}`, {
-    //     method: "GET",
-    //     headers: { "Content-Type": "application/json" }
-    // })
-    //     .then(res => res.json())
-    //     .then(data => (allResources = data))
-    //     .then(allResources => {
-    //         console.log(allResources);
-    //         renderResources(allResources);
-    //     })
-    //     .catch(console.error);
-    // //API CALL**************
+	// //API CALL. CALL FUNCTION ON READY**************
+	// fetch(`/resources?topic=${topicValue}`, {
+	//     method: "GET",
+	//     headers: { "Content-Type": "application/json" }
+	// })
+	//     .then(res => res.json())
+	//     .then(data => (allResources = data))
+	//     .then(allResources => {
+	//         console.log(allResources);
+	//         renderResources(allResources);
+	//     })
+	//     .catch(console.error);
+	// //API CALL**************
 
-    //API CALL. CALL FUNCTION ON READY**************
-    fetch(`/resources/${topicValue}`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" }
-    })
-        .then(res => res.json())
-        .then(data => (allResources = data))
-        .then(allResources => {
-            console.log(allResources);
-            renderResources(allResources);
-        })
-        .catch(console.error);
-    //API CALL**************
+	//API CALL. CALL FUNCTION ON READY**************
+	fetch(`/resources/${topicValue}`, {
+		method: "GET",
+		headers: { "Content-Type": "application/json" }
+	})
+		.then(res => res.json())
+		.then(data => (allResources = data))
+		.then(allResources => {
+			//console.log(allResources);
+			addResourcesToPage(allResources);
+		})
+		.catch(console.error);
+	//API CALL**************
 });
 
 document.getElementById("testButton").addEventListener("click", event => {
-    event.preventDefault();
-    // const body = {
-    //     topic: document.getElementById().value,
-    //     dateAdded: new Date(Date.now()).toUTCString(),
-    //     comment: document.getElementById().value,
-    //     resourceUrl: document.getElementById().value,
-    // };
+	event.preventDefault();
+	// const body = {
+	//     topic: document.getElementById().value,
+	//     dateAdded: new Date(Date.now()).toUTCString(),
+	//     comment: document.getElementById().value,
+	//     resourceUrl: document.getElementById().value,
+	// };
 
-    const body = {
-        topic: "nodeTest67578",
-        dateAdded: new Date(Date.now()).toUTCString(),
-        comment: "commentTest78678787",
-        resourceUrl: "www.urltesting.com"
-    };
+	const body = {
+		topic: "nodeTest67578",
+		dateAdded: new Date(Date.now()).toUTCString(),
+		comment: "commentTest78678787",
+		resourceUrl: "www.urltesting.com"
+	};
 
-    fetch("/resources", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body)
-    })
-        .then(res => res.json())
-        .then(data => (allData = data))
-        .then(allData => console.log(allData))
-        .catch(console.error);
+	fetch("/resources", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(body)
+	})
+		.then(res => res.json())
+		.then(data => (allData = data))
+		.then(allData => console.log(allData))
+		.catch(console.error);
 });
