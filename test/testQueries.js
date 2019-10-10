d4bea13f72d4f310eeb9de83204c4628794e8bcf
Lyndsey Scott <lyndseyscott@Lyndseys-MacBook-Pro.local>
2019-10-10 11:57:46 +0100

    test file add

---
+++ test/testQueries.js
@@ -0,0 +1,23 @@
+const test = require("tape");
+const deleteAllData = require("../database/dbBuild").deleteAllData;
+const populateDB = require("../database/dbBuild").populateDB;
+
+const initDB = require("../database/connection.js").initDB;
+const getDB = require("../database/connection.js").getDB;
+const closeDB = require("../database/connection.js").closeDB;
+
+const readResources = require("../database/queries/readResources");
+const createResources = require("../database/queries/createResources");
+
+
+test("initialising the database", t => {
+    initDB().then(db => {
+      deleteAllData(db)
+        .then(populateDB(db))
+        .then(closeDB)
+        .then(() => t.end());
+    });
+
+
+
+    
