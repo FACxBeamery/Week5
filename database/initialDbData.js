9fa3edcf6029665fa9a400011a8e2a601daa46d5
TKostrzewski <thomas_kostrzewski@hotmail.com>
2019-10-10 11:52:45 +0100

    Create data for initial db population

--- database/initialDbData.js
+++ database/initialDbData.js
@@ -1,6 +1,6 @@
 const initialData = [
     {
-        topic: "Git",
+        topic: "git",
         resourceUrl:
             "https://github.com/foundersandcoders/git-workflow-workshop-for-two",
         comment: "Intro to Git flow workshop",
@@ -13,47 +13,141 @@ const initialData = [
         dateAdded: Date.now()
     },
     {
-        topic: "Tape",
+        topic: "tape",
         resourceUrl: "https://github.com/foundersandcoders/testing-tdd-intro",
         comment: "Intro to testing and TDD",
         dateAdded: Date.now()
     },
     {
-        topic: "Tape",
+        topic: "tape",
         resourceUrl: "https://github.com/foundersandcoders/fizzbuzz",
         comment: "Tape (TDD) workshop",
         dateAdded: Date.now()
     },
     {
-        topic: "Pure functions",
+        topic: "functions",
         resourceUrl:
             "https://github.com/foundersandcoders/ws-pure-functions-easy-testing",
         comment: "Pure functions workshop",
         dateAdded: Date.now()
     },
     {
-        topic: "Docker",
+        topic: "docker",
         resourceUrl:
             "https://gist.github.com/arrested-developer/0e950b2b74b50e89b305cf6d9a7f28b5",
         comment: "Docker workshop",
         dateAdded: Date.now()
     },
     {
-        topic: "XHR",
+        topic: "xhr",
         resourceUrl: "https://github.com/foundersandcoders/api-workshop",
         comment: "Intro to XHR",
         dateAdded: Date.now()
     },
     {
-        topic: "XHR",
+        topic: "xhr",
         resourceUrl: "https://github.com/foundersandcoders/xhr-workshop",
         comment: "XHR workshop",
         dateAdded: Date.now()
     },
     {
-        topic: "Design",
+        topic: "design",
         resourceUrl: "https://github.com/foundersandcoders/design-bursts",
         comment: "Design Burst 1",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "software architecture",
+        resourceUrl:
+            "https://github.com/foundersandcoders/Workshop-Software-Architecture-Design",
+        comment: "Software architecture",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "software architecture",
+        resourceUrl:
+            "https://github.com/foundersandcoders/ws-software-design-js/tree/master/stage-1",
+        comment: "Software design workshop",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "node",
+        resourceUrl: "https://github.com/foundersandcoders/Node-Intro-Workshop",
+        comment: "Node HTTP server codealong",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "node",
+        resourceUrl: "https://github.com/node-girls/node-workshop",
+        comment: "Node girls HTTP server workshop (no frameworks)",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "functions",
+        resourceUrl:
+            "https://github.com/foundersandcoders/master-reference/blob/master/coursebook/week-2/intro-to-functions-slides.md",
+        comment: "Intro to JS functions",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "functions",
+        resourceUrl:
+            "https://github.com/foundersandcoders/morning-challenge-traffic-lights",
+        comment: "Traffic lights challenge",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "functions",
+        resourceUrl: "https://github.com/foundersandcoders/modules-challenge",
+        comment: "Modularisation challenge",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "express",
+        resourceUrl: "https://node-girls.gitbook.io/intro-to-express/",
+        comment: "Node girls Express workshop",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "error handling",
+        resourceUrl:
+            "https://github.com/foundersandcoders/error-handling-workshop",
+        comment: "Error handling workshop",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "linting",
+        resourceUrl:
+            "https://github.com/foundersandcoders/master-reference/blob/master/coursebook/week-5/linter.md",
+        comment: "Linting workshop",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "express",
+        resourceUrl:
+            "https://github.com/foundersandcoders/express-and-testing-workshop",
+        comment: "Testing routes in Express",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "api",
+        resourceUrl:
+            "https://github.com/foundersandcoders/mc-request-module-workshop",
+        comment: "Backend API call challenge",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "mongo",
+        resourceUrl:
+            "https://www.thepolyglotdeveloper.com/2019/01/getting-started-mongodb-docker-container-deployment/",
+        comment: "Mongo Shell",
+        dateAdded: Date.now()
+    },
+    {
+        topic: "express",
+        resourceUrl:
+            "https://itnext.io/how-to-share-a-single-database-connection-in-a-node-js-express-js-app-fcad4cbcb1e",
+        comment: "Optimising database connection in Express",
         dateAdded: Date.now()
     }
 ];
