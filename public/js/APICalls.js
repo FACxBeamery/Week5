61f26e5c9dd95f2e8b6dd0457753bdcc96adec52
Lyndsey Scott <lyndsey.736@gmail.com>
2019-10-11 13:39:06 +0100

    labels add for screenreader

--- public/js/APICalls.js
+++ public/js/APICalls.js
@@ -37,7 +37,7 @@ const postResourcesAPI = () => {
 		.then(res => res.json())
 		.then(data => (allData = data))
 		.then(allData => {
-			resourceAddConfirmation();
+			resourceAddOverlay();
 		})
 		.catch(console.error);
 };
