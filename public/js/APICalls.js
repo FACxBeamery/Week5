7f7072ee7a9865c2d0557aadab98e5d96248a01b
Lyndsey Scott <lyndseyscott@Lyndseys-MacBook-Pro.local>
2019-10-11 09:57:36 +0100

    user validation on search fix #13

--- public/js/APICalls.js
+++ public/js/APICalls.js
@@ -1,74 +1,43 @@
 const getResourcesAPI = idOfTopicField => {
-    const topicValue = document
-        .getElementById(idOfTopicField)
-        .value.toLowerCase();
-
-    fetch(`/resources/${topicValue}`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" }
-    })
-        .then(res => res.json())
-        .then(data => (allResources = data))
-        .then(allResources => {
-            if (allResources.length > 0) {
-                //console.log("last then", allResources);
-                addResourcesToPage(allResources);
-            } else {
-                const overlay = document.querySelector(".overlay");
-                overlay.style.display = "block";
-                const para = document.createElement("p");
-                para.classList.add("no-topic-message");
-                const node = document.createTextNode(
-                    "There are currently no resources for the topic you requested. Please do add some resources if you have them!"
-                );
-                para.appendChild(node);
-                overlay.appendChild(para);
-                const displayForm = document.getElementById(
-                    "section-add-topic"
-                );
-                displayForm.style.display = "block";
-                setTimeout(() => {
-                    while (overlay.firstChild) {
-                        overlay.removeChild(overlay.firstChild);
-                    }
-                    overlay.style.display = "none";
-                }, 3000);
-            }
-        })
-        .catch(console.error);
+	const topicValue = document
+		.getElementById(idOfTopicField)
+		.value.toLowerCase();
+	if (!topicValue) {
+		emptySearchOverlay();
+	} else {
+		fetch(`/resources/${topicValue}`, {
+			method: "GET",
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(res => res.json())
+			.then(data => (allResources = data))
+			.then(allResources => {
+				if (allResources.length > 0) {
+					addResourcesToPage(allResources);
+				} else {
+					noTopicOverLay();
+				}
+			})
+			.catch(console.error);
+	}
 };
 
 const postResourcesAPI = () => {
-    const body = {
-        topic: document.getElementById("topictoAdd").value.toLowerCase(),
-        dateAdded: Date.now(),
-        comment: document.getElementById("commentToAdd").value,
-        resourceUrl: document.getElementById("urlToAdd").value
-    };
-    fetch("/resources", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body)
-    })
-        .then(res => res.json())
-        .then(data => (allData = data))
-        .then(allData => {
-            console.log(allData);
-            const overlay = document.querySelector(".overlay");
-            overlay.style.display = "block";
-            const para = document.createElement("p");
-            para.classList.add("post-success-message");
-            const node = document.createTextNode(
-                `Thank you for adding a resorce! ${JSON.stringify(allData)}`
-            );
-            para.appendChild(node);
-            overlay.appendChild(para);
-            setTimeout(() => {
-                while (overlay.firstChild) {
-                    overlay.removeChild(overlay.firstChild);
-                }
-                overlay.style.display = "none";
-            }, 3000);
-        })
-        .catch(console.error);
+	const body = {
+		topic: document.getElementById("topictoAdd").value.toLowerCase(),
+		dateAdded: Date.now(),
+		comment: document.getElementById("commentToAdd").value,
+		resourceUrl: document.getElementById("urlToAdd").value
+	};
+	fetch("/resources", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(body)
+	})
+		.then(res => res.json())
+		.then(data => (allData = data))
+		.then(allData => {
+			resourceAddConfirmation();
+		})
+		.catch(console.error);
 };
--- public/js/DOMManip.js
+++ public/js/DOMManip.js
@@ -1,13 +1,13 @@
 const addResourcesToPage = resources => {
-    resetResourcesContainer();
-    const resourcesContainer = document.getElementById("resources-container");
-    resourcesContainer.id = "resources-container";
+	resetResourcesContainer();
+	const resourcesContainer = document.getElementById("resources-container");
+	resourcesContainer.id = "resources-container";
 
-    resources.forEach(resource => {
-        resourcesContainer.appendChild(renderResources(resource));
-        const displayForm = document.getElementById("section-add-topic");
-        displayForm.style.display = "block";
-    });
+	resources.forEach(resource => {
+		resourcesContainer.appendChild(renderResources(resource));
+		const displayForm = document.getElementById("section-add-topic");
+		displayForm.style.display = "block";
+	});
 };
 
 //render resources on page
@@ -17,43 +17,97 @@ const addResourcesToPage = resources => {
 // add new resource to page
 
 const renderResources = resource => {
-    // const resourceForm = document.createElement("form");
-    const resourceDiv = document.createElement("div");
-    resourceDiv.setAttribute("id", "resource-result-div");
+	// const resourceForm = document.createElement("form");
+	const resourceDiv = document.createElement("div");
+	resourceDiv.setAttribute("id", "resource-result-div");
 
-    const resourceURL = document.createElement("a");
-    resourceURL.setAttribute("href", resource.resourceUrl);
-    resourceURL.textContent = resource.resourceUrl;
-    resourceURL.setAttribute("id", "resource-url");
+	const resourceURL = document.createElement("a");
+	resourceURL.setAttribute("href", resource.resourceUrl);
+	resourceURL.textContent = resource.resourceUrl;
+	resourceURL.setAttribute("id", "resource-url");
 
-    const resourceComment = document.createElement("p");
-    resourceComment.textContent = `Comments: ${resource.comment}`;
-    resourceComment.setAttribute("id", "resource-comment");
+	const resourceComment = document.createElement("p");
+	resourceComment.textContent = `Comments: ${resource.comment}`;
+	resourceComment.setAttribute("id", "resource-comment");
 
-    const resourceTopic = document.createElement("p");
-    resourceTopic.textContent = `Topic: ${resource.topic}`;
-    resourceTopic.setAttribute("id", "resource-topic");
-    //resourceDiv.setAttribute("id", )
+	const resourceTopic = document.createElement("p");
+	resourceTopic.textContent = `Topic: ${resource.topic}`;
+	resourceTopic.setAttribute("id", "resource-topic");
+	//resourceDiv.setAttribute("id", )
 
-    // add URL, topic, comment from search here?
+	// add URL, topic, comment from search here?
 
-    //const editButton = document.createElement("button");
-    //editButton.textContent = "edit";
-    //event listener for clicking edit button
-    //add class to edit button?
-    resourceDiv.appendChild(resourceURL);
-    resourceDiv.appendChild(resourceComment);
-    resourceDiv.appendChild(resourceTopic);
+	//const editButton = document.createElement("button");
+	//editButton.textContent = "edit";
+	//event listener for clicking edit button
+	//add class to edit button?
+	resourceDiv.appendChild(resourceURL);
+	resourceDiv.appendChild(resourceComment);
+	resourceDiv.appendChild(resourceTopic);
 
-    //resourceDiv.appendChild(editButton);
+	//resourceDiv.appendChild(editButton);
 
-    // add classes for styling if needed
-    return resourceDiv;
+	// add classes for styling if needed
+	return resourceDiv;
 };
 
 const resetResourcesContainer = () => {
-    const resourcesContainer = document.getElementById("resources-container");
-    while (resourcesContainer.firstChild) {
-        resourcesContainer.removeChild(resourcesContainer.firstChild);
-    }
+	const resourcesContainer = document.getElementById("resources-container");
+	while (resourcesContainer.firstChild) {
+		resourcesContainer.removeChild(resourcesContainer.firstChild);
+	}
+};
+
+const noTopicOverLay = () => {
+	const overlay = document.querySelector(".overlay");
+	overlay.style.display = "block";
+	const para = document.createElement("p");
+	para.classList.add("no-topic-message");
+	const node = document.createTextNode(
+		"There are currently no resources for the topic you requested. Please do add some resources if you have them!"
+	);
+	para.appendChild(node);
+	overlay.appendChild(para);
+	const displayForm = document.getElementById("section-add-topic");
+	displayForm.style.display = "block";
+	setTimeout(() => {
+		while (overlay.firstChild) {
+			overlay.removeChild(overlay.firstChild);
+		}
+		overlay.style.display = "none";
+	}, 3000);
+};
+
+const emptySearchOverlay = () => {
+	const overlay = document.querySelector(".overlay");
+	overlay.style.display = "block";
+	const para = document.createElement("p");
+	para.classList.add("no-search-message");
+	const node = document.createTextNode("Please enter a topic to search by!");
+	para.appendChild(node);
+	overlay.appendChild(para);
+	const displayForm = document.getElementById("section-add-topic");
+	displayForm.style.display = "block";
+	setTimeout(() => {
+		while (overlay.firstChild) {
+			overlay.removeChild(overlay.firstChild);
+		}
+		overlay.style.display = "none";
+	}, 3000);
+};
+
+const resourceAddConfirmation = () => {
+	const overlay = document.querySelector(".overlay");
+	overlay.style.display = "block";
+	const para = document.createElement("p");
+	para.classList.add("post-success-message");
+	const node = document.createTextNode(`Thank you for adding a resource!}`);
+	para.appendChild(node);
+	overlay.appendChild(para);
+	setTimeout(() => {
+		while (overlay.firstChild) {
+			overlay.removeChild(overlay.firstChild);
+		}
+		overlay.style.display = "none";
+	}, 3000);
 };
